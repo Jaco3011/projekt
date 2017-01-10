2ebe02a1ac2a686ab9aeb7101f8d3b5d390e659f
Jaco3011 <jacekhozej@gmail.com>
2017-01-10 13:31:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ using namespace std ;
 
 int main () {
   string sciezka ;
-  cout << "Podaj ścieżkę do pliku" << endl ;
+  cout << "Podaj ścieżkę do plików" << endl ;
   cin >> sciezka ;
-  fstream baza ;
-  baza.open(sciezka.c_str(), ios::in | ios::out) ;
-  if (baza.is_open()) {
+  fstream baza1 ;
+  fstream baza2 ;
+  fstream baza3 ;
+  baza1.open((sciezka.c_str()+"jeden.txt"), ios::in | ios::out) ;
+  if (baza1.is_open()) {
     cout << "Otwarte" << endl ;
   } else {
     cout << "Problemy z otwarciem" << endl ;
   } ;
-  baza.flush() ;
-  baza.close() ;
+  baza1.flush() ;
+  baza1.close() ;
   return 0 ;
 } ;
