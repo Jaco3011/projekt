353868ef41e246410851895acf0c7f7705569cff
Jaco3011 <jacekhozej@gmail.com>
2017-01-10 13:11:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream.h>
 #include <string>
 #include <fstream>
+
 using namespace std ;
+
 int main () {
   string sciezka ;
   cout >> "Podaj ścieżkę do pliku" << endl ;
   cin >> sciezka ;
   fstream baza(sciezka, ios::in | ios::out) ;
-  
+  baza.open() ;
+  if (baza.is_open()) {
+    cout << "Otwarte" << endl ;
+  } else {
+    cout << "Problemy z otwarciem" << endl ;
+  }
+  baza.close() ;
   return 0 ;
 } ;
