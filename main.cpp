9aaedbacd8d20068e5c91166149291b4962bad3c
Jaco3011 <jacekhozej@gmail.com>
2017-01-10 13:09:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 using namespace std ;
 int main () {
+  string sciezka ;
+  cout >> "Podaj ścieżkę do pliku" << endl ;
+  cin >> sciezka ;
+  fstream baza(sciezka, ios::in | ios::out) ;
   
   return 0 ;
 } ;
