caab074bde17a7cc204023e4211a5773e82f114e
Jaco3011 <jacekhozej@gmail.com>
2017-01-17 13:56:48 +0100

    Update funkcje.hpp

--- funkcje.hpp
+++ funkcje.hpp
@@ -1,6 +1,6 @@
 #ifndef funkcje_hpp
 #define funkcje_hpp
-#include <iostream.h>
+#include <iostream>
 #include <string>
 #include <fstream>
 using namespace std ;
@@ -10,42 +10,42 @@ string sciezkanowa() {
   cin >> a ;
   return a ;
 } ;
-bool wszystkodobrze(int a, fstream * dane){
+bool wszystkodobrze(int a, fstream * dane[]){
     for(int i=0; i<a; i++){
-        if(!dane[i].good()){
+        if(!(*dane[i]).good()){
           return false ;
         };
     };
     return true ;
 };
-void wczytajbaze (int a, fstream * pliki, string * sciezki){
+void wczytajbaze (int a, fstream * pliki[], string * sciezki[]){
     for(int i=0; i<a; i++){
-        pliki[i].open(sciezki[i].c_str(), ios::in|ios::out) ;
+        (*pliki[i]).open((*sciezki[i]).c_str(), ios::in|ios::out) ;
     };
 };
-void wczytywanie(fstream * dane, string * sciezka, string * dokad){
-  dane.open(sciezka.c_str()), ios::in) ;
-   if (dane.is_open()) {
+void wczytywanie(fstream * dane, string * sciezka, string * dokad[]){
+  (*dane).open((*sciezka).c_str(), ios::in) ;
+   if ((*dane).is_open()) {
     cout << "Wczytywanie ścieżek do plików" << endl ;
     int i=0 ;
-    while (i<3 && !dane.eof()) {
-      getline(dane, dokad[i]) ;
+    while (i<3 && !(*dane).eof()) {
+      getline((*dane), (*dokad[i])) ;
       i++ ;
     } ;
-    if (dokad[2]=="" && dane.eof()) {
+    if ((*dokad[2])=="" && (*dane).eof()) {
       cout << "Ścieżki nie zostały wczytanie" << endl ;
     } else {
       cout << "Wczytano ścieżki dostępu" << endl ;
     } ;
-  dane.clear(goodbit) ;
-  dane.close() ;
+  (*dane).clear() ;
+  (*dane).close() ;
   } else {
     cout << "Problemy z otwarciem ścieżek do plików" << endl ;
   } ;
 } ; //koniec wczytywania
 void WypiszMenu(bool czy){
   cout << "Menu główne bazy danych" << endl ;
-  if(poprawnie){
+  if(czy){
     cout << "Baza danych wczytana" << endl ;
   } else {
     cout << "Baza danych nie jest wczytana" << endl ;
