1c92574cb4e571aa9a09255a9d39d94c3dfb766b
Jaco3011 <jacekhozej@gmail.com>
2017-01-17 13:31:32 +0100

    Update funkcje.hpp

--- funkcje.hpp
+++ funkcje.hpp
@@ -23,4 +23,33 @@ void wczytajbaze (int a, fstream * pliki, string * sciezki){
         pliki[i].open(sciezki[i].c_str(), ios::in|ios::out) ;
     };
 };
+void wczytywanie(fstream * dane, string * sciezka, string * dokad){
+  dane.open(sciezka.c_str()), ios::in) ;
+   if (dane.is_open()) {
+    cout << "Wczytywanie ścieżek do plików" << endl ;
+    int i=0 ;
+    while (i<3 && !dane.eof()) {
+      getline(dane, dokad[i]) ;
+      i++ ;
+    } ;
+    if (dokad[2]=="" && dane.eof()) {
+      cout << "Ścieżki nie zostały wczytanie" << endl ;
+    } else {
+      cout << "Wczytano ścieżki dostępu" << endl ;
+    } ;
+  dane.clear(goodbit) ;
+  dane.close() ;
+  } else {
+    cout << "Problemy z otwarciem ścieżek do plików" << endl ;
+  } ;
+} ; //koniec wczytywania
+void WypiszMenu(bool czy){
+  cout << "Menu główne bazy danych" << endl ;
+  if(poprawnie){
+    cout << "Baza danych wczytana" << endl ;
+  } else {
+    cout << "Baza danych nie jest wczytana" << endl ;
+  } ;
+  cout << "Opcje:" << endl ;
+} ;
 #endif
