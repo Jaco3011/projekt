27325e382471b7e37d3e508df237d35e301f2254
Jaco3011 <jacekhozej@gmail.com>
2017-02-10 19:04:51 +0100

    Create klasy.cpp

---
+++ klasy.cpp
@@ -0,0 +1,41 @@
+#define klasy_cpp
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <cstdlib>
+#include <conio.h>
+#include <stdio.h>
+#include <queue>
+using namespace std ;
+bool klient::czysty() {
+    return this->itemki.empty() ;
+} ;
+void klient::TenKlient(){
+    cout << "Imię: " << this->imie << endl ;
+    cout << "Nazwisko: "<< this->nazwisko << endl ;
+    cout << "wiek: " << this->wiek << endl ;
+} ;
+void wypozyczalnia::DodajKlienta(klient a){
+  przedmioty.push_back(a) ;
+} ;
+bool wypozyczalnia::IstnenieKlienta(int a) {
+    if (ludzie.a==NULL){
+      return false ;
+    } else {
+      return true ;
+    } ;
+} ;
+void wypozyczalnia::UsunKlienta(int a) {
+    this->ludzie.erase(a) ;
+  } ;
+void wypozyczalnia::DodajKlienta(klient a) {
+    this->ludzie.push_back(a)
+ } ;
+void wypozyczalnia::Zmien(corobic ab, int cd){
+    zmiana yyy;
+    yyy.ab=ab ;
+    yyy.cd=cd ;
+    this->zmienianie.push(yyy) ;
+  } ;
