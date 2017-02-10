f26899bbf6375428adeb100dc940013bbf2e79d3
Jaco3011 <jacekhozej@gmail.com>
2017-02-10 17:37:24 +0100

    Update funkcje.hpp

--- funkcje.hpp
+++ funkcje.hpp
@@ -4,75 +4,9 @@
 #include <string>
 #include <fstream>
 using namespace std ;
-string sciezkanowa() {
-  string a ;
-  cout << "Podaj sciezke do plików" << endl ;
-  cin >> a ;
-  return a ;
-} ;
-bool wszystkodobrze(int a, fstream * dane[]){
-    for(int i=0; i<a; i++){
-        if(!(*dane[i]).good()){
-          return false ;
-        };
-    };
-    return true ;
-};
-void wczytajbaze (int a, fstream * pliki[], string * sciezki[]){
-    for(int i=0; i<a; i++){
-        (*pliki[i]).open((*sciezki[i]).c_str(), ios::in|ios::out) ;
-    };
-};
-bool wczytywanie(fstream * dane, string * sciezka, string * dokad[]){
-  bool wynik ;
-  (*dane).open((*sciezka).c_str(), ios::in) ;
-   if ((*dane).is_open()) {
-    cout << "Wczytywanie ścieżek do plików" << endl ;
-    int i=0 ;
-    while (i<3 && !(*dane).eof()) {
-      getline((*dane), (*dokad[i])) ;
-      i++ ;
-    } ;
-    if ((*dokad[2])=="" && (*dane).eof()) {
-      cout << "Ścieżki nie zostały wczytanie" << endl ;
-      wynik=false ;
-    } else {
-      cout << "Wczytano ścieżki dostępu" << endl ;
-      wynik=true ;
-    } ;
-  (*dane).clear() ;
-  } else {
-    cout << "Problemy z otwarciem ścieżek do plików" << endl ;
-     wynik=false ;
-  } ;
-  (*dane).close() ;
-  return wynik ;
-} ; //koniec wczytywania
-void WypiszMenu(bool czy){
-  cout << "Menu główne bazy danych" << endl ;
-  if(czy){
-    cout << "Baza danych wczytana" << endl ;
-  } else {
-    cout << "Baza danych nie jest wczytana" << endl ;
-  } ;
-  cout << "Opcje:" << endl ;
-  if (czy) {
-    cout << "Q - wczytaj bazę danych jeszcze raz (z obecnych ścieżek)" << endl ;
-    cout << "W - podaj nowy adres do bazy danych (i zamknij obecną)" << endl ;
-    cout << "E - zamknij bazę danych" << endl ;
-    cout << "R - dodaj klienta" << endl ;
-    cout << "T - usuń klienta" << endl ;
-    cout << "Y - modyfikuj dane klienta" << endl ;
-    cout << "U - wypożycz przedmiot" << endl ;
-    cout << "I - zwróc przedmiot" << endl ;
-    cout << "O - sprawdź przedmiot" << endl ;
-    cout << "P - inwentarz" << endl ;
-    cout << "A - lista klientów" << endl ;
-    cout << "S - zapisz bazę danych" << endl ;
-    cout << "D - sprawdź klienta" << endl ;
-  } else {
-  cout << "Q - spróbuj wczytać bazę danych jeszcze raz (z obecnych ścieżek)" << endl ;
-  cout << "W - podaj nowy adres do bazy danych" << endl ;
-  } ;
-} ;
+string sciezkanowa()
+bool wszystkodobrze(int a, fstream * dane[])
+void wczytajbaze (int a, fstream * pliki[], string * sciezki[])
+bool wczytywanie(fstream * dane, string * sciezka, string * dokad[])
+void WypiszMenu(bool czy)
 #endif
