aeb443e10e886bc3dee43ec7ef8f346057081caf
Jaco3011 <jacekhozej@gmail.com>
2017-02-14 11:56:41 +0100

    Update funkcje.hpp

--- funkcje.hpp
+++ funkcje.hpp
@@ -4,10 +4,11 @@
 #include <string>
 #include <fstream>
 using namespace std ;
-string sciezkanowa()
-bool wszystkodobrze(int a, fstream * dane[])
-void wczytajbaze (int a, fstream * pliki[], string * sciezki[])
-bool wczytywanie(fstream * dane, string * sciezka, string * dokad[])
-void WypiszMenu(bool czy)
-void DawajInty(List * <int> liczby, string s)
+string sciezkanowa() ;
+bool wszystkodobrze(int a, fstream * dane[]) ;
+void wczytajbaze (int a, fstream * pliki[], string * sciezki[]) ;
+bool wczytywanie(fstream * dane, string * sciezka, string * dokad[]) ;
+void WypiszMenu(bool czy) ;
+void DawajInty(List * <int> liczby, string s) ;
+void przesun (int n, fstream * plik) ;
 #endif
